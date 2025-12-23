082104f26cef29a017e25454bf14c86eb5378902
Pranjal <pranjalpatel0903@gmail.com>
2025-12-23 12:15:39 +0530

    Final Push

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODDE_ENV != "production") { //only use our .env file in development phase != in production phase
+if(process.env.NODE_ENV != "production") { //only use our .env file in development phase != in production phase
     require('dotenv').config()
 }
 
@@ -10,6 +10,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError.js")
 const session = require("express-session")
+const MongoStore = require("connect-mongo").default
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
@@ -19,7 +20,7 @@ const listingRouter = require("./routes/listing.js")
 const reviewRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+const dbUrl = process.env.ATLASDB_URL
 
 main()
     .then(() => {
@@ -28,7 +29,7 @@ main()
     .catch((err) => console.log(err))
 
 async function main() {
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(dbUrl)
 }
 
 app.set("view engine", "ejs")
@@ -38,20 +39,34 @@ app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, "/public")))
 app.engine("ejs", ejsMate)
 
+//mongo store
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600
+})
+
+store.on("error", (err) => { // FIXED: Added parameter
+    console.log("ERROR in MONGO SESSION STORE", err)
+})
+
 const sessionOptions = {
-    secret: "mysupersecretcode", 
+    store: store,
+    secret: process.env.SECRET, 
     resave: false, 
     saveUninitialized: true,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 *60 * 60 * 1000,
-        httpOnly: true //for security purposes, to prevent from XSS attacks(cross-scripting attacks)
+        maxAge: 7 * 24 * 60 * 60 * 1000, // FIXED: Added missing asterisk
+        httpOnly: true
     }
 }
 
 // app.get("/", (req, res) => {
 //     res.send("Hi, I am root.")
-// })
+// })   
 
 app.use(session(sessionOptions)) //to use session
 app.use(flash())
